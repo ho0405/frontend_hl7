c5e9a8b323a2e2030d04a754cc499701b9cb5a5f
Ryan Back <ho0405@gmail.com>
2024-04-13 16:22:08 -0600

    link are updated

--- app/penguinChatbot/footer.js
+++ app/penguinChatbot/footer.js
@@ -12,10 +12,10 @@ const Footer = ({ isDarkMode }) => {
         <p className={`text-lg font-bold mb-4 ${isDarkMode ? 'text-white' : 'text-gray-900'}`}>Hi7 Hl7</p>
         <p>&copy; 2024 Pureform & Pure kids Radiology</p>
         <div className="flex mt-4">
-          <a href="https://www.facebook.com" className={`mr-4 ${socialIconClass}`}><FaFacebookF /></a>
-          <a href="https://www.instagram.com" className={`mr-4 ${socialIconClass}`}><FaInstagram /></a>
-          <a href="https://www.twitter.com" className={`mr-4 ${socialIconClass}`}><FaTwitter /></a>
-          <a href="https://www.youtube.com" className={`mr-4 ${socialIconClass}`}><FaYoutube /></a>
+          <a href="https://www.facebook.com/PureformPureKidsRadiology/" className={`mr-4 ${socialIconClass}`}><FaFacebookF /></a>
+          <a href="https://www.instagram.com/pureformradiology/?hl=en" className={`mr-4 ${socialIconClass}`}><FaInstagram /></a>
+          <a href="https://twitter.com/i/flow/login?redirect_after_login=%2Fmypureform" className={`mr-4 ${socialIconClass}`}><FaTwitter /></a>
+          <a href="https://www.youtube.com/channel/UCQjumPZiPAnPyybbQD8sr7A" className={`mr-4 ${socialIconClass}`}><FaYoutube /></a>
         </div>
       </div>
       <div className="flex flex-1 justify-around flex-wrap md:flex-nowrap">
