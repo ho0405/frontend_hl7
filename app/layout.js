cafa87273f8b78b800955f68a10480c61c34afa0
Ryan Back <ho0405@gmail.com>
2024-04-15 12:05:33 -0600

    deploy issues

--- app/layout.js
+++ app/layout.js
@@ -2,7 +2,7 @@
 import { Inter } from 'next/font/google'
 import './globals.css'
 import { AuthContextProvider } from './context/AuthContext'
-import '../styles/globals.css';
+
 
 const inter = Inter({ subsets: ['latin'] })
 
